558b31c2a560f078bba659186b67d2d84ddfc0f9
Agente OpenGravity <agente@vibranium.com>
2026-07-23 20:05:33 +0200

    Initial PWA commit

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE='affirm-v2';
+const ASSETS=['./index.html','./manifest.json','./musica_loop.mp3'];
+self.addEventListener('install',e=>{
+e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+self.skipWaiting();
+});
+self.addEventListener('activate',e=>{
+e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+self.clients.claim();
+});
+self.addEventListener('fetch',e=>{
+e.respondWith(fetch(e.request).then(r=>{
+if(r&&r.status===200){
+const clone=r.clone();
+caches.open(CACHE).then(c=>c.put(e.request,clone));
+}
+return r;
+}).catch(()=>caches.match(e.request)));
+});
