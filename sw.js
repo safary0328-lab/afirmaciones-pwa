d76d3212cf6858590b070550306dccb0389f1733
Agente OpenGravity <agente@vibranium.com>
2026-08-25 20:34:31 +0200

    fix: actualizar versión de caché para forzar actualización

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='affirm-v4';
+const CACHE='affirm-v5';
 const ASSETS=['./index.html','./manifest.json','./musica_loop.mp3'];
 self.addEventListener('install',e=>{
 e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
